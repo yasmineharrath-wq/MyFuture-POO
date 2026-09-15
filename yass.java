bc3e15c6bd77000493ba1c4a66c43da07a96c415
unknown <yasmine.harrath@edu.isetcom.tn>
2026-09-15 15:12:30 +0100

    TPO : Configuration de l'environnement Visual Studio et JDK

---
+++ yass.java
@@ -0,0 +1,8 @@
+public class yass{
+    public static void main (String[]args){
+        System.out.println("=====================================");
+        System.out.println("environnement visual studio + java pret !");
+        System.out.println("=====================================");
+
+    }
+}
